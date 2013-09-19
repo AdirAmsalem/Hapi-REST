33874341e0c366332d32a23d2bcb7a99c9e04a7f
Adir Amsalem <adiramsalem@hotmail.com>
2013-09-19 23:34:57 +0300

    Reverted MongoDB to a local server and added MongoDB service to travis' configuration

--- init_db.js
+++ init_db.js
@@ -1,6 +1,6 @@
 // Load modules
 var fs = require('fs');
-var db = require('mongoskin').db('adircdn.com:27017/hapi-rest', { safe: false });
+var db = require('mongoskin').db('localhost:27017/hapi-rest', { safe: false });
 
 // Initialize 'users' collection
 var usersData = JSON.parse(fs.readFileSync(__dirname + '/app/models/data/users.json', 'utf8'));
