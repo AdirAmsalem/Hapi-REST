95b9c27da5ac63d157843a5ca5f871d054c1274b
Adir Amsalem <adiramsalem@hotmail.com>
2013-10-18 19:17:06 +0300

    moved db_init.js to server folder

--- server/init_db.js
+++ server/init_db.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var db = require('mongoskin').db('localhost:27017/hapi-rest', { safe: false });
 
 // Initialize 'users' collection
-var usersData = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
+var usersData = JSON.parse(fs.readFileSync('../data/users.json', 'utf8'));
 db.collection('users').remove();
 db.collection('users').insert(usersData, function(error) {
 	if (!error) {
