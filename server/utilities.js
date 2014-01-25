95ff7e7e8f6df40633b6546c27b076f77f47bf29
Adir Amsalem <adiramsalem@hotmail.com>
2014-01-25 13:02:53 +0200

    Server - minor improve to utilities file

--- server/utilities.js
+++ server/utilities.js
@@ -2,58 +2,57 @@
 var app = require('./config');
 var db = app.getLib('mongoskin');
 
-/**
- * Handle incoming requests
- * 
- * @param  {Object} req
- * @param  {Object} promise
- */
-function handleResponse(req, promise) {
-	promise.then(
-		// Success
-		function(res) {
-			req.reply(res);
-		},
-		// Error
-		function(code) {
-			req.reply().code(code);
-		}
-	);
-}
+// Public methods
+var methods = {
+	/**
+	 * Handle incoming requests
+	 * 
+	 * @param  {Object} req
+	 * @param  {Object} promise
+	 */
+	handleResponse: function(req, promise) {
+		promise.then(
+			// Success
+			function(res) {
+				req.reply(res);
+			},
+			// Error
+			function(code) {
+				req.reply().code(code);
+			}
+		);
+	},
 
-/**
- * Converts and validates the specified id
- * 
- * @param  {String} id
- * @return {String|Boolean} converted id on success / false on failure
- */
-function convertId(id) {
-	try {
-		return db.ObjectID.createFromHexString(id);
-	} catch(e) {
-		return false;
-	}
-}
-
-/**
- * Check if the given object is empty
- * 
- * @param  {Object}  obj
- * @return {Boolean} true/false
- */
-function isEmptyObject(obj) {
-	for (var key in obj) {
-		if (obj.hasOwnProperty(key)) {
+	/**
+	 * Converts and validates the specified id
+	 * 
+	 * @param  {String} id
+	 * @return {String|Boolean} converted id on success / false on failure
+	 */
+	convertId: function(id) {
+		try {
+			return db.ObjectID.createFromHexString(id);
+		} catch(e) {
 			return false;
 		}
+	},
+
+	/**
+	 * Check if the given object is empty
+	 * 
+	 * @param  {Object}  obj
+	 * @return {Boolean} true/false
+	 */
+	isEmptyObject: function(obj) {
+		for (var key in obj) {
+			if (obj.hasOwnProperty(key)) {
+				return false;
+			}
+		}
+		return true;
 	}
-	return true;
-}
+};
 
 
 // Publish API
-module.exports = {
-	handleResponse: handleResponse,
-	convertId: convertId,
-	isEmptyObject: isEmptyObject
-};
+module.exports = methods;
